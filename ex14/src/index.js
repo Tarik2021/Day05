f32fb9505618c8ca6f17e6a3d9ae710f84eff66f
Tarik2021 <tariks2021@gmail.com>
2021-03-13 07:30:07 +0100

    Day05 exercises

---
+++ ex14/src/index.js
@@ -0,0 +1,22 @@
+function main() {
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+  
+    var wordBlanks =
+      "Really " +
+      myAdjective +
+      " " +
+      myNoun +
+      " " +
+      myVerb +
+      " " +
+      myAdverb +
+      ".";
+  
+      return wordBlanks;
+  }
+  console.log(main());
+  module.exports = main;
+  
